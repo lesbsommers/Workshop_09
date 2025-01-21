082709429498e138a62453a2634446a66ea50d1b
lesommers <lesbsommers@gmail.com>
2025-01-20 19:49:52 -0800

    commiting the success.html and success.js files

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+//form.html submit button output//
+const form = document.querySelector('#success')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+});
---
+++ success.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    window.location.href = window.location./success.html
+  }, 5000)
